23d2748faa9c2597a33f58bb357ab04ed4e6bd7f
mac <gnl96@naver.com>
2022-08-10 14:12:54 +0900

    add_product_page

--- app/src/main/java/com/example/shoppi_android/shoppiandroid/MainActivity.kt
+++ app/src/main/java/com/example/shoppi_android/shoppiandroid/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println("뭐")
+        setContentView(R.layout.activity_product_detail)
+
     }
 }
