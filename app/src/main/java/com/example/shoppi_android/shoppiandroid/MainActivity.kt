dfa986b6cb1fad42fcb688d632f3963cb17c658b
mac <gnl96@naver.com>
2022-08-29 14:44:38 +0900

    8.29 하단 네비게이션 추가

--- app/src/main/java/com/example/shoppi_android/shoppiandroid/MainActivity.kt
+++ app/src/main/java/com/example/shoppi_android/shoppiandroid/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.shoppi_android.shoppiandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarItemView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tranjaction=supportFragmentManager.beginTransaction()
+        tranjaction.add(R.id.container_main,FragmentHome())
+        tranjaction.commit()
+
+
+        val bottomNavigationView =findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList=null
+
 
     }
 }
